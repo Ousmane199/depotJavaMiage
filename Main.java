a90537526baf1b26bd03b317ff09ea433715c8a5
Ousmane199 <106654657+Ousmane199@users.noreply.github.com>
2025-09-09 14:04:03 +0200

    Modification avant la pause

--- Main.java
+++ Main.java
@@ -28,7 +28,7 @@ public class Main {
     {
         System.out.println("Veuillez saisir un entier");
         Scanner sc = new Scanner(System.in);
-        return sc.nextInt();
+        return sc.nextDouble();
     }
 
     static void verifierSaisie3(){
